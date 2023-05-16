ff39af3489e69fc33f493f62105cf9148ef479e7
Jorge Santana <jorge.santana@inkluziva.org>
2023-05-16 11:40:21 -0300

    config: sets app port on .env

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.use("/", routes);
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
